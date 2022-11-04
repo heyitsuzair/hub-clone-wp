2e66430cbf988647defe9f747e712efc606ec8b3
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-05 02:03:35 +0500

    Header Init

--- template-parts/header/MainHeader.php
+++ template-parts/header/MainHeader.php
@@ -26,7 +26,7 @@
     </div>
     <div class="col-9 header-right">
         <div class="header-desktop">
-            <?php wp_nav_menu() ?>
+            <?php wp_nav_menu(); ?>
         </div>
     </div>
 </div>
