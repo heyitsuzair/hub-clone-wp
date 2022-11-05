9876620f9633a0a6488b2c5f8796ebbfd2c3aa19
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-06 00:20:48 +0500

    Post Card Header Completed

--- template-parts/blog/entry-header.php
+++ template-parts/blog/entry-header.php
@@ -6,18 +6,20 @@
  * @package HUB_WP
  */
 
+
+
 ?>
 
 <?php
 if (is_home()) {
 ?>
-<header class="d-flex align-items-center gap-3 entry-header post-<?php the_ID();  ?>-header">
+<header class="d-flex align-items-center justify-content-between gap-3 entry-header post-<?php the_ID();  ?>-header">
     <span class="blog-card-badge">
         <?php
             the_category();
             ?>
     </span>
-    <span>2 Years Ago</span>
+    <span class="date-published"><?php echo get_the_date('Y-m-d'); ?></span>
 </header>
 <?php
 }
