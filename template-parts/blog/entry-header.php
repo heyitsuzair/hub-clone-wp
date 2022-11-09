327aafdfe43eff9555d5139a93337d235c67f967
heyitsuzair <uzairdevil354123@gmail.com>
2022-11-10 03:55:43 +0500

    Fixes

--- template-parts/blog/entry-header.php
+++ template-parts/blog/entry-header.php
@@ -23,11 +23,10 @@ $post_moral = do_shortcode('[get_post_moral post_id=' . get_the_ID() . ']');
 ?>
 
 <?php
-<<<<<<< HEAD
+
 if (is_home()  || is_search()) {
-=======
-if (is_home() || is_search()) {
->>>>>>> search-page
+
+
 ?>
 <header class="d-flex align-items-center justify-content-between gap-3 entry-header post-<?php the_ID();  ?>-header">
     <span class="blog-card-badge">
